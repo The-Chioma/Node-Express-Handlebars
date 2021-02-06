85dbc9a8b04b2208d71d6891810f0c4d35d4a85b
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-02-06 22:33:29 +0000

    created database and inserted values into the table in schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Beef', false);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken', false);
+INSERT INTO burgers (name, devoured) VALUES ('Ham', false);
